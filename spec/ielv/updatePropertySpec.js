23ffa3e7bec93da5706ae887f269b82dde88bf92
Nathan Calvank <nwcalvank@gmail.com>
2019-02-16 21:51:52 -0800

    Check for existing property before other updates

--- spec/ielv/updatePropertySpec.js
+++ spec/ielv/updatePropertySpec.js
@@ -1,7 +1,11 @@
 import fs from 'fs';
 import MockAdapter from 'axios-mock-adapter';
 
-import updateProperty from '../../src/ielv/updateProperty';
+import updateProperty, {
+  NOT_FOUND,
+  getProperty,
+  putDescription,
+} from '../../src/ielv/updateProperty';
 import { myVRClient } from '../../src/api/client';
 
 // Mock JSON Response Data
@@ -11,15 +15,29 @@ import myVRRoom from '../mockData/myvr/room.json';
 
 // Initialize the custom axios instance
 const MOCK_PROPERTY_ID = 1234;
+const MOCK_PROPERTY_NAME = 'Mock Property';
+const MOCK_PROPERTY_EXTERNAL_ID = `IELV_${MOCK_PROPERTY_ID}`;
 const mockMyVRClient = new MockAdapter(myVRClient);
 
+// GET Stubs
+mockMyVRClient
+  .onGet(`/properties/${MOCK_PROPERTY_EXTERNAL_ID}/`)
+  .reply(200, myVRProperty);
+mockMyVRClient
+  .onGet(`/properties/${MOCK_PROPERTY_EXTERNAL_ID}567/`)
+  .reply(200, NOT_FOUND);
+
 // PUT Stubs
+const updatedDescription = ielvProperty.title[0];
 const tmpProperty = Object.assign(myVRProperty, {
-  description: ielvProperty.title[0],
+  description: updatedDescription,
 });
 mockMyVRClient
-  .onPut(`/properties/IELV_${MOCK_PROPERTY_ID}/`)
+  .onPut(`/properties/${MOCK_PROPERTY_EXTERNAL_ID}/`)
   .reply(200, tmpProperty);
+mockMyVRClient
+  .onPut(`/properties/${MOCK_PROPERTY_EXTERNAL_ID}567/`)
+  .reply(200, NOT_FOUND);
 
 // POST Stubs
 mockMyVRClient.onPost(`/rooms/`).reply(200, myVRRoom);
@@ -28,6 +46,42 @@ describe('updateProperty', () => {
   it('should call the MyVR API with a payload and return the updated property', () => {
     updateProperty(ielvProperty).then(data => {
       expect(data).toEqual([tmpProperty, myVRRoom]);
+    });
+  });
+});
+
+describe('getProperty', () => {
+  it('should call the MyVR API and return the matching property', () => {
+    getProperty(`IELV_${MOCK_PROPERTY_ID}`).then(data => {
+      expect(data).toEqual(myVRProperty);
+    });
+  });
+
+  it('should call the MyVR API and return Not Found when the property does not exist', () => {
+    getProperty(`IELV_${MOCK_PROPERTY_ID}567`).then(data => {
+      expect(data).toEqual(NOT_FOUND);
+    });
+  });
+});
+
+describe('putDescription', () => {
+  it('should call the MyVR API and return the updated property', () => {
+    putDescription({
+      name: MOCK_PROPERTY_NAME,
+      description: updatedDescription,
+      externalId: MOCK_PROPERTY_EXTERNAL_ID,
+    }).then(data => {
+      expect(data).toEqual(tmpProperty);
+    });
+  });
+
+  it('should call the MyVR API and return an error message if the request fails', () => {
+    putDescription({
+      name: MOCK_PROPERTY_NAME,
+      description: updatedDescription,
+      externalId: `${MOCK_PROPERTY_EXTERNAL_ID}567`,
+    }).then(data => {
+      expect(data).toEqual(NOT_FOUND);
     });
   });
 });
--- src/ielv/updateProperty.js
+++ src/ielv/updateProperty.js
@@ -1,35 +1,65 @@
 import { myVRClient } from '../api/client';
 
-export default ({
+export const NOT_FOUND = 'Not Found';
+
+export const getProperty = externalId =>
+  myVRClient
+    .get(`/properties/${externalId}/`)
+    .then(({ data }) => data)
+    .catch(
+      ({ response: { status } }) => (status === 404 ? NOT_FOUND : 'Other error')
+    );
+
+export const putDescription = ({ name, description, externalId }) =>
+  myVRClient
+    .put(`/properties/${externalId}/`, {
+      // required
+      name,
+      // relevant payload
+      description,
+      // becomes null if not set explicitly
+      externalId,
+    })
+    .then(({ data }) => data)
+    .catch(
+      // TODO: Improve this error message
+      ({ response: { status } }) =>
+        status === 404 ? NOT_FOUND : `Status: ${status}`
+    );
+
+// TODO: Make this less naive
+export const postBedrooms = externalId =>
+  myVRClient
+    .post('/rooms/', {
+      property: externalId,
+    })
+    .then(({ data }) => data)
+    .catch(console.log);
+
+export default async ({
   id: [ielvId],
   title: [name],
   description: [ielvDescription],
 }) => {
   const externalId = `IELV_${ielvId}`;
   // GET property current details
+  let property = await getProperty(externalId);
 
-  // POST new properties? Is that possible?
+  if (property === NOT_FOUND) {
+    // POST new property
+    // TODO: implement
+    // property = myVRClient.post()
+  }
 
   // PUT all details (sans bedrooms) into main description section
-  const description = myVRClient
-    .put(`/properties/${externalId}/`, {
-      // required
-      name,
-      // relevant payload
-      description: ielvDescription,
-      // becomes null if not set explicitly
-      externalId,
-    })
-    .then(({ data }) => data)
-    .catch(console.log);
+  const description = putDescription({
+    name,
+    description: ielvDescription,
+    externalId,
+  });
 
   // POST new bedrooms
-  const bedrooms = myVRClient
-    .post('/rooms/', {
-      property: externalId,
-    })
-    .then(({ data }) => data)
-    .catch(console.log);
+  const bedrooms = postBedrooms(externalId);
 
   return Promise.all([description, bedrooms]);
 };
