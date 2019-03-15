95d7859cac73bd79f3b2a7f6b09ae24e1147a154
Nathan Calvank <nwcalvank@gmail.com>
2019-03-14 19:55:38 -0700

    Update logging behaviour
    This removes logging (via custom log method) when running in a test
    environment.

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,10 +1,14 @@
 import { TEST_ENV, DEV_ENV } from '../globals';
 
 // alias console methods
-export const { log, warn } = console;
+const { log: _log } = console;
 
 // Environment-dependent logging
 export const logError = msg => {
   if (TEST_ENV || DEV_ENV) throw new Error(msg);
-  else log(msg);
+  else _log(msg);
+};
+
+export const log = msg => {
+  if (!TEST_ENV) _log(msg);
 };
